0219db090ba4e99bf18c4850b9a078b992a7840b
santiethan27 <106702996+santiethan27@users.noreply.github.com>
2024-02-25 19:27:17 -0500

    Actualizacion

--- src/database.js
+++ src/database.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/patitasDB");
+        await mongoose.connect("mongodb+srv://admin:admin@patitasdb.mpxr73j.mongodb.net/?retryWrites=true&w=majority&appName=PatitasDB");
+        //mongodb://127.0.0.1:27017/patitasDB // LOCAL
+        //mongodb+srv://admin:admin@patitasdb.mpxr73j.mongodb.net/?retryWrites=true&w=majority&appName=PatitasDB // NUBE
         console.log(">>> DB(Ok)");
     } catch (error) {
         console.log(error);
